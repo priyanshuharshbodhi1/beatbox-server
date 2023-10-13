c1d8c7a4daae171140e396a8bee92550f8775774
priyanshu <priyanshuqpwp@gmail.com>
2023-10-13 21:22:47 +0530

    created login api by jwt

--- index.js
+++ index.js
@@ -2,54 +2,91 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
-const app = express();
+const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const path = require("path");
 const PORT = process.env.PORT || 3500;
 dotenv.config();
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("./public"));
 
 const User = require("./models/user.js");
 
-
-
+// APIs
 
 app.get("/health", (req, res) => {
   res.json({ message: "All good!" });
 });
 
-
+//signup api
 app.post("/api/signup", async (req, res) => {
-    try {
-      const { name, mobile, email, password } = req.body;
-      if (!password) {
-        return res.status(400).json({ message: 'Password is required' });
-      }
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      let user = await User.findOne({ email });
-      if (user) {
-        return res.json({ message: "User already exists" });
-      } else {
-        const newUser = new User({
-          name,
-          mobile,
-          email,
-          password: hashedPassword,
-        });
-        await newUser.save();
-        return res.redirect(302, "http://localhost:3000");
-        console.log("User Created Successfully");
-      }
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: "An error occurred", error: error.message });
+  try {
+    const { name, mobile, email, password } = req.body;
+    if (!password) {
+      return res.status(400).json({ message: "Password is required" });
     }
-  });
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.json({ message: "User already exists" });
+    } else {
+      const newUser = new User({
+        name,
+        mobile,
+        email,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      return res.redirect(302, "http://localhost:3000");
+      console.log("User Created Successfully");
+    }
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
+  }
+});
+
+//login api
+app.post("/api/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      const passwordMatched = await bcrypt.compare(password, user.password);
+      if (passwordMatched) {
+        const jwToken = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
+          expiresIn: 6000,
+        });
+        res.cookie("jwt", jwToken, { httpOnly: true });
+        res.redirect(302, "http://localhost:3000");
+        return;
+      } else {
+        res.json({
+          status: "FAIL",
+          message: "Incorrect password",
+        });
+      }
+    } else {
+      res.json({
+        status: "FAIL",
+        message: "User does not exist",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      status: "FAIL",
+      message: "Something went wrong",
+      error,
+    });
+  }
+});
 
 app.listen(PORT, () => {
   mongoose
