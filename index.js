2734ad5d663ad5bc89a48dac68b168c0c18bf17c
priyanshu <priyanshuqpwp@gmail.com>
2023-10-13 21:11:26 +0530

    created signup api

--- index.js
+++ index.js
@@ -1,39 +1,62 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const dotenv = require('dotenv')
-const path = require('path')
-dotenv.config()
-const PORT = process.env.PORT || 3500
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const app = express();
+const dotenv = require("dotenv");
+const path = require("path");
+const PORT = process.env.PORT || 3500;
+dotenv.config();
 
-// app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("./public"));
 
-// app.use('/', require('./routes/root'))
+const User = require("./models/user.js");
 
-// app.all('*', (req, res) => {
-//     res.status(404)
-//     if (req.accepts('html')) {
-//         res.sendFile(path.join(__dirname, 'views', '404.html'))
-//     } else if (req.accepts('json')) {
-//         res.json({ message: '404 Not Found' })
-//     } else {
-//         res.type('txt').send('404 Not Found')
-//     }
-// })
 
-app.get('/health', (req, res) => {
-    res.json({ message: 'All good!' })
-  })
 
-  app.listen(PORT, () => {
-    mongoose
-      .connect(process.env.MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() =>
-        console.log(`Server running on http://localhost:${PORT}`)
-      )
-      .catch((error) => console.error(error));
-  });
+
+app.get("/health", (req, res) => {
+  res.json({ message: "All good!" });
+});
+
+
+app.post("/api/signup", async (req, res) => {
+    try {
+      const { name, mobile, email, password } = req.body;
+      if (!password) {
+        return res.status(400).json({ message: 'Password is required' });
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
   
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.json({ message: "User already exists" });
+      } else {
+        const newUser = new User({
+          name,
+          mobile,
+          email,
+          password: hashedPassword,
+        });
+        await newUser.save();
+        return res.redirect(302, "http://localhost:3000");
+        console.log("User Created Successfully");
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "An error occurred", error: error.message });
+    }
+  });
+
+  
+
+app.listen(PORT, () => {
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log(`Server running on http://localhost:${PORT}`))
+    .catch((error) => console.error(error));
+});
---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: "string", required: true },
+  mobile: { type: "string", required: true},
+  email: { type: "string", required: true, unique: true },
+  password: { type: "string", required: true },
+});
+
+module.exports = mongoose.model("User", userSchema);
+
+
