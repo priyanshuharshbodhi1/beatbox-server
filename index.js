692190516e250081beef49f7c75745abb9b4c975
priyanshu <priyanshuqpwp@gmail.com>
2023-10-14 12:21:23 +0530

    created logout functionality

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 const PORT = process.env.PORT || 3500;
@@ -15,30 +16,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("./public"));
 app.use(cookieParser());
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 const User = require("./models/user.js");
 const Product = require("./models/product.js");
 
-//Middlewares
-const isAuthenticated = (req, res, next) => {
-  const token = req.cookies.jwt;
+// APIs------------------------------------------
 
-  if (!token) {
-    return res.sendStatus(401);
-  }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
-      return res.sendStatus(403);
-    }
-
-    req.user = user;
-
-    next();
-  });
-};
-
-// APIs
-
+//health api
 app.get("/health", (req, res) => {
   res.json({ message: "All good!" });
 });
@@ -110,17 +101,43 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
+//Middlewares
+const isAuthenticated = (req, res, next) => {
+    const token = req.cookies.jwt;
+    console.log(token);
+  
+    if (!token) {
+      return res.sendStatus(401);
+    }
+  
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+  
+      req.user = user;
+  
+      next();
+    });
+  };
+
 //isloggedin api
 app.get("/api/isloggedin", isAuthenticated, (req, res) => {
     // Check if the user is logged in and include the user's firstName in the response
-    const isLoggedIn = true; 
-    if (isLoggedIn) {
-      res.json({ isLoggedIn: true, name: req.user.name });
+    if (req.user) {
+      res.json({ isLoggedIn: true});
     } else {
       res.json({ isLoggedIn: false });
     }
   });
   
+//logout api
+app.post("/api/logout", (req, res) => {
+  // Clear the JWT token from cookies by setting an expired token
+  res.cookie("jwt", "", { expires: new Date(0) });
+
+  res.status(200).json({ message: "Logged out successfully" });
+});
 
 app.get("/products", async (req, res) => {
   try {
