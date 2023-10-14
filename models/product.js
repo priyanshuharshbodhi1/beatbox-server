a0fb9019ebea01c822218efcf2b107d6438f391c
priyanshu <priyanshuqpwp@gmail.com>
2023-10-14 17:36:52 +0530

    changed the products model

--- models/product.js
+++ models/product.js
@@ -4,15 +4,15 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   company: { type: String, required: true },
   type: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: String, required: true },
   color: { type: String, required: true },
-  rating: { type: Number, required: true },
-  totalReviews: { type: Number, required: true },
-  tagLine: { type: String, required: true },
+  rating: { type: String, required: true },
+  reviews: { type: String, required: true },
+  tagline: { type: String, required: true },
   about: { type: String, required: true },
   stock: { type: String, required: true },
-  images: [{ type: String, required: true }],
-  featured: { type: Boolean, required: true},
+  images: { type: Object, required: true },
+  featured: { type: String, required: true},
 });
 
 module.exports = mongoose.model("Product", productSchema);
